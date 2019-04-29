09f401088ba975b3f1174d5e31d5775c3409290f
kpetersn <kmpeters@anl.gov>
2019-04-29 11:56:32 -0500

    Added the WithoutDevSup IOC from PR #1 to pigcs2IOC. Changed the name of the binary so it doesn't conflict with the IOC from PR #1.

---
+++ iocs/pigcs2IOC/iocBoot/iocPIGCS2NoDevSup/st.C867.cmd
@@ -0,0 +1,24 @@
+#!../../bin/linux-x86_64/pigcsWithoutDevSup
+
+< envPaths
+
+dbLoadDatabase("${TOP}/dbd/pigcs2NoDevSup.dbd")
+pigcs2NoDevSup_registerRecordDeviceDriver(pdbbase)
+
+drvAsynIPPortConfigure("tcpip_c867","10.0.9.72:50000",0,0,0)
+asynOctetConnect("tcpip_c867", "tcpip_c867")
+asynOctetSetInputEos("tcpip_c867",0,"\n")
+
+PI_GCS2_CreateController("PIasyn2", "tcpip_c867", 2, 0,0, 10, 250)
+
+dbLoadTemplate("asynMotor.substitutions.c867")
+
+#asynSetTraceIOMask("tcpip_c867", -1 , 0x2)
+#asynSetTraceMask("tcpip_c867", -1, 0x09)
+
+# Initialize the IOC and start processing records
+iocInit()
+
+#asynSetTraceIOMask("tcpip_c867", -1 , 0x2)
+#asynSetTraceMask("tcpip_c867", -1, 0x09)
+
---
+++ iocs/pigcs2IOC/iocBoot/iocPIGCS2NoDevSup/st.E727.cmd
@@ -0,0 +1,24 @@
+#!../../bin/linux-x86_64/pigcsWithoutDevSup
+
+< envPaths
+
+dbLoadDatabase("${TOP}/dbd/pigcs2NoDevSup.dbd")
+pigcs2NoDevSup_registerRecordDeviceDriver(pdbbase)
+
+drvAsynIPPortConfigure("tcpip_e727","10.0.9.74:50000",0,0,0)
+asynOctetConnect("tcpip_e727", "tcpip_e727")
+asynOctetSetInputEos("tcpip_e727",0,"\n")
+
+PI_GCS2_CreateController("PIasyn3", "tcpip_e727",3, 0,0, 10, 250)
+
+dbLoadTemplate("asynMotor.substitutions.e727")
+
+#asynSetTraceIOMask("tcpip_e727", -1 , 0x2)
+#asynSetTraceMask("tcpip_e727", -1, 0x09)
+
+#asynReport 10 “PIasyn3”
+
+iocInit
+
+#asynSetTraceIOMask("tcpip_e727", -1 , 0x2)
+#asynSetTraceMask("tcpip_e727", -1, 0x09)
